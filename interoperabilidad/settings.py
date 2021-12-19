94f0805ae1c2d1d3d61a61f95be62b8fd1cc7e40
Jhonathan Salas <jhonssegura@outlook.com>
2021-12-18 21:59:44 -0500

    Cambiando a DB Postgres

--- interoperabilidad/settings.py
+++ interoperabilidad/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'interoperabilidad.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'issuing_system',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
